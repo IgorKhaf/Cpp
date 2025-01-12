ca0ebe110b467ad624f587ea4ff5aee8b594216f
Igor Khaf <106630021+IgorKhaf@users.noreply.github.com>
2025-01-11 19:48:54 -0800

    First commit

---
+++ HelloWorld/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "Hi there!" << std::endl;
+  std::cout << "How are you!" << std::endl;
+  std::cout << "I'm good" << std::endl;
+
+  return 0;
+}
