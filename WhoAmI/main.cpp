302ca61c2df8282415cb34ee161eb73c76f6d084
Igor Khaf <106630021+IgorKhaf@users.noreply.github.com>
2025-01-15 13:53:04 -0800

    Added description to the images

--- WhoAmI/main.cpp
+++ WhoAmI/main.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
-void showImage(const String&);
+using namespace cv;
+using namespace std;
+
+void showImage(const String&, const String&);
 
 int main() {
-    showImage("image.jpg");
-    showImage("image.jpg");
-    showImage("image.jpg");
-    showImage("image.jpg");
+  
+  int choice = 0;
+  do {
+    std::cout << "Ask me about: \n";
+    std::cout << "1. Where I am from. \n";
+    std::cout << "2. My first university. \n";
+    std::cout << "3. The place I like to visit. \n";
+    std::cout << "4. Exit. \n";
+    std::cin >> choice;
+    if (choice == 1){
+      std::cout << "I am from Saint Peterburg Russia. \n";
+      showImage("/Users/igorkhafizov/Documents/College/Winter_2025/C++/WhoAmI/image1.jpg", "Saint Peterburg Russia");
+    }
+    else if (choice == 2){
+      std::cout << "I drop off the university of Aerospace and Instrumentation after three years of study. \n";
+      showImage("/Users/igorkhafizov/Documents/College/Winter_2025/C++/WhoAmI/image2.jpg", "University of Aerospace and Instrumentation");
+    }
+    else if (choice == 3){
+      showImage("/Users/igorkhafizov/Documents/College/Winter_2025/C++/WhoAmI/image3.jpeg", "Alki Beach Seattle");
+    }
+    else if (choice == 4){
+      break;
+    }
+    else {
+      std::cout << "Invalid choice. \n";
+    }
+  } while (choice !=4);
+  
+  
+    // showImage("image.jpg");
     return 0;
 }
 
-void showImage(const String& jpgName)
+void showImage(const String& jpgName, const String& text)
 {
   destroyWindow("PicWindow");
 
@@ -22,7 +52,15 @@ void showImage(const String& jpgName)
     return;
   }
 
+  Point origin(50, 50);
+  int fontFace = FONT_HERSHEY_SIMPLEX;
+  double fontScale = 1;
+  Scalar color(0,255,0);
+  int thickness = 2;
+
+  putText(image, text, origin, fontFace, fontScale, color, thickness);
+
   namedWindow("PicWindow", WINDOW_AUTOSIZE);
-  inshow("PicWindow", image);
-  waitKey(50);
+  imshow("PicWindow", image);
+  waitKey(0);
 }
