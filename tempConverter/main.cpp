96bcd11baeaa0b124273ee6673afe96c66d8e84e
Igor Khaf <106630021+IgorKhaf@users.noreply.github.com>
2025-01-15 11:45:12 -0800

    Refactor temperature conversion prompts and remove unnecessary comments

--- tempConverter/main.cpp
+++ tempConverter/main.cpp
@@ -2,20 +2,19 @@
 
 int main() {
 
-  // variable declaration
+
   int f, c;
   char choice;
 
-  // user input
+
   std::cout << "Do you want to convert from Fahrenheit to Celsius? (Y/N - if otherwise): ";
   std::cin >> choice;
 
-  // if user wants to convert from Fahrenheit to Celsius
+
   if (choice == 'Y' || choice == 'y') {
     std::cout << "Temperature in Fahrenheit; ";
     std::cin >> f;
 
-    // formula for conversion
     c = (f - 32) * 5 / 9;
 
     std::cout << f << " degrees Fahrenheit = " << c << " degrees Celsius " << std::endl;}
@@ -23,7 +22,7 @@ int main() {
     std::cout << "Temperature in Celsius: ";
     std::cin >> c;
 
-    // formula for conversion
+
     f = (c * 9 / 5) + 32;
     
     std::cout << c << " degrees Celsius = " << f << " degrees Fahrenheit" << std::endl;
